966e5b0562727bf1b7bce4eec1458a7c70f80385
Eric Backhage <eric.backhage@gmail.com>
2016-10-13 22:16:36 +0200

    Possible to get hash of a file using MD5 or SHA1

---
+++ FileHasher.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace SigningTool
+{
+    public class FileHasher
+    {
+        private readonly string AlgorithmIdentifier;
+        public enum Algorithms
+        {
+            MD5,
+            SHA1
+        }
+        
+        public FileHasher(Algorithms algorithm)
+        {
+            switch (algorithm)
+            {
+                case Algorithms.MD5:
+                    AlgorithmIdentifier = "MD5";
+                    break;
+                case Algorithms.SHA1:
+                    AlgorithmIdentifier = "SHA1";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public string GetHash(string path)
+        {
+            byte[] hash;
+            // Wrapping the file stream in a buffered stream improves performance for large files.
+            using (var input = new BufferedStream(File.OpenRead(path), 1200000))
+            {
+                hash = HashAlgorithm.Create(AlgorithmIdentifier).ComputeHash(input);
+            }
+            var sb = new StringBuilder();
+            foreach (var b in hash)
+            {
+                sb.AppendFormat("{0:X2}", b);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tests/FileHasherTests.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SigningTool;
+
+namespace Tests
+{
+    [TestClass]
+    public class FileHasherTests
+    {
+        [TestMethod]
+        public void TestMD5Hash()
+        {
+            var tempFile = Path.GetTempFileName();
+            using (var outputFile = new StreamWriter(tempFile))
+            {
+                outputFile.WriteLine("Hash calculated using MD5.");
+            }
+            var fileHasher = new FileHasher(FileHasher.Algorithms.MD5);
+            var hash = fileHasher.GetHash(tempFile);
+            const string expectedHash = "41286C0D6E1CB9BA6FED0BE0CEC399FE"; // Calculated using onlinemd5.com
+
+            Assert.AreEqual(expectedHash, hash);
+        }
+
+        [TestMethod]
+        public void TestSHA1Hash()
+        {
+            var tempFile = Path.GetTempFileName();
+            using (var outputFile = new StreamWriter(tempFile))
+            {
+                outputFile.WriteLine("Hash calculated using SHA1.");
+            }
+            var fileHasher = new FileHasher(FileHasher.Algorithms.SHA1);
+            var hash = fileHasher.GetHash(tempFile);
+            const string expectedHash = "C561579D37ED07ABF663F5413808EAFC7EF2C4C5";
+
+            Assert.AreEqual(expectedHash, hash);
+        }
+    }
+}
