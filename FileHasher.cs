d5460a629552a17d06fcded88a22eba19fe42c1e
Eric Backhage <eric.backhage@gmail.com>
2016-10-13 22:39:28 +0200

    Made the GetHash method static.
    No need to create an instance of the FileHasher class.

--- FileHasher.cs
+++ FileHasher.cs
@@ -7,35 +7,19 @@ namespace SigningTool
 {
     public class FileHasher
     {
-        private readonly string AlgorithmIdentifier;
-        public enum Algorithms
+        public enum Algorithm
         {
             MD5,
             SHA1
         }
         
-        public FileHasher(Algorithms algorithm)
-        {
-            switch (algorithm)
-            {
-                case Algorithms.MD5:
-                    AlgorithmIdentifier = "MD5";
-                    break;
-                case Algorithms.SHA1:
-                    AlgorithmIdentifier = "SHA1";
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-
-        public string GetHash(string path)
+        public static string GetHash(string path, Algorithm algorithm)
         {
             byte[] hash;
             // Wrapping the file stream in a buffered stream improves performance for large files.
             using (var input = new BufferedStream(File.OpenRead(path), 1200000))
             {
-                hash = HashAlgorithm.Create(AlgorithmIdentifier).ComputeHash(input);
+                hash = HashAlgorithm.Create(algorithm.ToString()).ComputeHash(input);
             }
             var sb = new StringBuilder();
             foreach (var b in hash)
--- Tests/FileHasherTests.cs
+++ Tests/FileHasherTests.cs
@@ -15,8 +15,7 @@ namespace Tests
             {
                 outputFile.WriteLine("Hash calculated using MD5.");
             }
-            var fileHasher = new FileHasher(FileHasher.Algorithms.MD5);
-            var hash = fileHasher.GetHash(tempFile);
+            var hash = FileHasher.GetHash(tempFile, FileHasher.Algorithm.MD5);
             const string expectedHash = "41286C0D6E1CB9BA6FED0BE0CEC399FE"; // Calculated using onlinemd5.com
 
             Assert.AreEqual(expectedHash, hash);
@@ -30,8 +29,7 @@ namespace Tests
             {
                 outputFile.WriteLine("Hash calculated using SHA1.");
             }
-            var fileHasher = new FileHasher(FileHasher.Algorithms.SHA1);
-            var hash = fileHasher.GetHash(tempFile);
+            var hash = FileHasher.GetHash(tempFile, FileHasher.Algorithm.SHA1);
             const string expectedHash = "C561579D37ED07ABF663F5413808EAFC7EF2C4C5";
 
             Assert.AreEqual(expectedHash, hash);
